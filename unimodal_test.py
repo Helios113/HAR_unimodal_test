0fdb00cd050ee327eda8907c329316112a4cfa60
Preslav Aleksandrov <2248797a@student.gla.ac.uk>
2022-06-28 19:21:19 +0100

    first commit

---
+++ unimodal_test.py
@@ -0,0 +1,11 @@
+# What I need to achieve here?
+# 1. Load the data from mHealth
+# Should be the easiest part
+# 1.1 Add the folder - 
+# Just import everything in a tensors
+# A
+# 2. Replicate the unimodal test from Multimodal Federated Learning on IoT Data
+
+# Code begins
+# imports
+from torchdata.datapipes.iter import FileLister, FileOpener, StreamReader
